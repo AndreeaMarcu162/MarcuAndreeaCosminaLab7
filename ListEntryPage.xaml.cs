60e74573c535bcfebcc6a11722daa4cb9def459c
Andreea C. Marcu <Andreea C. Marcu@DESKTOP-UT0GNG5>
2024-11-27 03:02:16 +0200

    lab8

---
+++ ListEntryPage.xaml.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using SQLite;
+using MarcuAndreeaCosminaLab7.Data;
+using System.IO;
+
+namespace MarcuAndreeaCosminaLab7;
+
+public partial class ListEntryPage : ContentPage
+{
+    public ListEntryPage()
+    {
+        InitializeComponent();
+    }
+    protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+        listView.ItemsSource = await App.Database.GetShopListsAsync();
+    }
+    async void OnShopListAddedClicked(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new ListPage
+        {
+            BindingContext = new ShopList()
+        });
+    }
+    async void OnListViewItemSelected(object sender, SelectedItemChangedEventArgs e)
+    {
+        if (e.SelectedItem != null)
+        {
+            await Navigation.PushAsync(new ListPage
+            {
+                BindingContext = e.SelectedItem as ShopList
+            });
+        }
+    }
+}
